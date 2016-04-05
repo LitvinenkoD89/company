1ce3c05f3aa1c17fedf0040c8671cac19afec194
Litvinenko <torerolda@gmail.com>
2016-04-05 09:49:53 +0300

    fix

--- example.rb
+++ example.rb
@@ -1,97 +1,97 @@
 class Employee
 
 	def initialize(first_name, last_name)
-		setFirstName(first_name)
-		setLastName(last_name)
+		@first_name=first_name
+		@last_name=last_name
 	end
 	
-	def setFirstName(first_name)
+	def first_name=(first_name)
 		@first_name = first_name
 	end
 
-	def setLastName(last_name)
+	def last_name=(last_name)
 		@last_name = last_name
 	end
 
-	def getFirstName
-		return @first_name
+	def first_name
+		@first_name
 	end
 	
-	def getLastName
-		return @last_name
+	def last_name
+		@last_name
 	end	
 end
 
 class Company
 
 	def initialize(title, bik, industry)
-		_setTitle(title)
-		_setBik(bik)
-		_setIndustry(industry)
+		@title = title
+		@bik = bik
+		@industry = industry
 		@employees = Array.new
 	end
 
-	def addNewEmployee(first_name, last_name)
-		employee = Employee.new(first_name, last_name)
-		@employees.push(employee)
+	def add_new_employee(first_name, last_name)
+    key = check_exist_employee(first_name, last_name)
+
+    if key == nil
+		  employee = Employee.new(first_name, last_name)
+		  @employees.push(employee)
+    else
+      puts "Employee already exist"
+    end
 	end
 
-	def showEmployees
-		
-		@employees.each{
-			|employee|
-			puts employee.getFirstName + ' ' + employee.getLastName
-		}
+	def show_employees
+		@employees.each do |employee|
+			puts employee.first_name + ' ' + employee.last_name
+		end
 	end
 
-	def fireEmployee(first_name, last_name)
-		key = checkExistEmployee(first_name, last_name)
+	def fire_employee(first_name, last_name)
+		key = check_exist_employee(first_name, last_name)
 
 		if key != nil
 			@employees.delete_at(key)
 		else
-			return "Employee not exist"
+			puts "Employee not exist"
 		end	
 	end
 
-	def checkExistEmployee(first_name, last_name)
-		employee = Employee.new(first_name, last_name)
-
-		return @employees.index(employee)
+	def check_exist_employee(first_name, last_name)
+		return @employees.index{ |employee| employee.first_name == first_name && employee.last_name == last_name }
 	end	
 
-	def getTitle()
-		return @title
+	def title
+		@title
 	end
 
-	def getBik()
-		return @bik
+	def bik
+		@bik
 	end	
 
-	def getIndustry
-		return @industry
+	def industry
+		@industry
 	end	
 
-	private 
-		def _setTitle(title)
-			@title = title
-		end
-		
-		def _setBik(bik)
-			@bik = bik
-		end	
+	def title=(title)
+		@title = title
+	end
+	
+	def bik=(bik)
+		@bik = bik
+	end	
 
-		def _setIndustry(industry)
-			@industry = industry
-		end	
+	def industry=(industry)
+		@industry = industry
+	end	
 end
 
 company = Company.new('test_company', 12313532, 'IT')
-company.addNewEmployee('Ivanov', 'Ivan')
-company.addNewEmployee('Petrov', 'Petr')
-company.showEmployees
-company.checkExistEmployee('Ivanov', 'Ivan')
-company.fireEmployee('Sidorov', 'Sidor')
-company.showEmployees
-company.fireEmployee('Ivanov', 'Ivan')
-company.showEmployees
+company.add_new_employee('Ivanov', 'Ivan')
+company.add_new_employee('Ivanov', 'Ivan')
+company.add_new_employee('Petrov', 'Petr')
+company.show_employees
+company.fire_employee('Petrov', 'ivan')
+company.fire_employee('Petrov', 'Petr')
+company.show_employees
